b0d31813e273f86132121447196d76ff3a97dc5a
Kohei Kumegawa <kohei.kumegawa@gmail.com>
2022-09-07 16:09:00 +0900

    Update scA2_completeArchRProj.R

--- code/scA2_completeArchRProj.R
+++ code/scA2_completeArchRProj.R
@@ -11,17 +11,18 @@ pre_arc <- readRDS("rds/pre_arc.rds")
 arc <- pre_arc[which(pre_arc$TSSEnrichment >= 8 & pre_arc$nFrags >= 3000)]
 
 #clustering
-arc <- addIterativeLSI(arc, useMatrix = "TileMatrix", name = "IterativeLSI") %>%
-  addClusters(., reducedDims = "IterativeLSI", maxClusters = 30, force = T) %>%
-  addUMAP(., reducedDims = "IterativeLSI", force = T)
+arc <- addIterativeLSI(arc, useMatrix = "TileMatrix", name = "IterativeLSI", force = T) %>%
+       addClusters(., reducedDims = "IterativeLSI", force = T) %>%
+       addUMAP(., reducedDims = "IterativeLSI", force = T)
 
 #call peaks
 pathToMacs2 <- findMacs2()
 arc <- addGroupCoverages(arc, groupBy = "Clusters") %>%
-  addReproduciblePeakSet(., groupBy = "Clusters", 
-                         pathToMacs2 = pathToMacs2, 
-                         method = "q", cutOff = 0.05) %>% 
-  addPeakMatrix(.)
+       addReproduciblePeakSet(., groupBy = "Clusters", 
+                                 pathToMacs2 = pathToMacs2, 
+                                 shift = -40, extsize = 80,
+                                 method = "q", cutOff = 0.05) %>% 
+       addPeakMatrix(.)
 
 #calculate motif deviation
 arc <- addMotifAnnotations(arc, motifSet = "homer", annoName = "homer")
