f85d1a18e4f4ed7f2c23c08a73238bbc3f49ea8e
Kohei Kumegawa <kohei.kumegawa@gmail.com>
2022-09-13 14:01:05 +0900

    Create scC2_completeArchRProj.R

---
+++ code/module3/scC2_completeArchRProj.R
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------------------
+# scC2_completeArchRProj.R
+#----------------------------------------------------------------------------
+#Kohei Kumegawa, Japanese Foundation for Cancer Research
+library(ArchR)
+library(dplyr)
+addArchRThreads(threads = 8)
+
+#filter cells
+arc <- readRDS("rds/arc.rds")
+
+#clustering
+arc <- addIterativeLSI(arc, useMatrix = "TileMatrix", name = "IterativeLSI", force = T) %>%
+  addClusters(., reducedDims = "IterativeLSI", force = T) %>%
+  addUMAP(., reducedDims = "IterativeLSI", force = T)
+
+#call peaks
+pathToMacs2 <- findMacs2()
+arc <- addGroupCoverages(arc, groupBy = "Clusters") %>%
+  addReproduciblePeakSet(., groupBy = "Clusters", 
+                         pathToMacs2 = pathToMacs2, 
+                         shift = -40, extsize = 80,
+                         method = "q", cutOff = 0.05) %>% 
+  addPeakMatrix(.)
+
+#calculate motif(homer) deviation
+arc <- addMotifAnnotations(arc, motifSet = "homer", annoName = "homer")
+arc <- addBgdPeaks(arc) %>% addDeviationsMatrix(., peakAnnotation = "homer")
+
+#save rds
+saveRDS(arc, "rds/arc.rds")
